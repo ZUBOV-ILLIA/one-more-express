985a49ac13d2d653c1d620f199ad0aac883c1fe8
ZUBOV ILLIA <zubovillia@gmail.com>
2024-08-22 18:56:37 +0300

    added DB json, trying to work with json database directly, using fs read/write file

--- api/index.js
+++ api/index.js
@@ -1,31 +1,24 @@
 const express = require("express");
-const cors = require('cors');
-// const path = require('path');
 const app = express();
+const cors = require('cors');
+const path = require('path');
+const fs = require('fs');
+
+let todos = fs.readFileSync(path.join(__dirname, 'DB.json'), 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+});
+
+todos = JSON.parse(todos);
+
+console.log(todos, typeof todos);
+
 const port = 3001;
+// let { todos, db } = require('./DB');
 
-let db = {
-  users: [
-    { id: 1, username: 'Frodo Begins', role: 'hobbit', skill: 'blue sword' },
-    { id: 2, username: 'Samwise Gamgee', role: 'hobbit', skill: 'friendship' },
-    { id: 3, username: 'Gendalf', role: 'wizard', skill: 'magic' },
-    { id: 4, username: 'Legolas', role: 'elf', skill: 'archery' },
-    { id: 5, username: 'Aragorn', role: 'human', skill: 'sword' }, 
-  ]
-}
 
-let todos = [
-  { userId: 1, id: 'fbbb8a42-df38-4ee4-afa6-1b62718693ff', title: 'Learn HTML', completed: false },
-  { userId: 1, id: 'fbbb8a42-df38-43e4-afa6-1b62718693ff', title: 'Learn CSS', completed: false },
-  { userId: 1, id: 'fbb18a42-df38-43e4-afa6-1b62718693ff', title: 'Learn JS', completed: true },
-  { userId: 2, id: 'fb3b8a42-df38-43e4-afa6-1b62718693ff', title: 'Learn React', completed: false },
-  { userId: 2, id: 'fb4b8a42-df38-43e4-afa6-1b62718693ff', title: 'Learn Redux', completed: false },
-  { userId: 3, id: 'fb5b8a42-df38-43e4-afa6-1b62718693ff', title: 'Learn Node.js', completed: false },
-  { userId: 4, id: 'fbeb8a42-df38-43e4-afa6-1b62718693ff', title: 'Learn Express', completed: false },
-  { userId: 5, id: 'fbab8a42-df38-43e4-afa6-1b62718693ff', title: 'Learn MongoDB', completed: false },
-  { userId: 5, id: 'fbzb8a42-df38-43e4-afa6-1b62718693ff', title: 'Learn SQL', completed: false },
-  { userId: 5, id: 'fbgg8a42-df38-43e4-afa6-1b62718693ff', title: 'Learn Python', completed: false },
-];
 
 app.use(cors());
 app.use(express.json());
@@ -62,6 +55,7 @@ app.post('/todo', (req, res, ) => {
 
   const newTodo = { userId, id, title: title.trim(), completed };
   todos.unshift(newTodo);
+  fs.writeFileSync(path.join(__dirname, 'DB.json'), JSON.stringify(todos));
 
   res.json(newTodo);
 });
@@ -84,6 +78,8 @@ app.post('/todos/:id', (req, res) => {
     return item;
   });
 
+  fs.writeFileSync(path.join(__dirname, 'DB.json'), JSON.stringify(todos));
+
   res.sendStatus(200);
 });
 
@@ -91,6 +87,8 @@ app.delete('/todos/:id', (req, res) => {
   const { id } = req.params;
 
   todos = todos.filter((item) => item.id !== id);
+
+  fs.writeFileSync(path.join(__dirname, 'DB.json'), JSON.stringify(todos));
 
   res.sendStatus(200);
 });
