d7143027607cb673d25c7714b836f68136524566
ZUBOV ILLIA <zubovillia@gmail.com>
2024-08-28 01:21:14 +0300

    updated removeMany function

--- api/services/todo.service.js
+++ api/services/todo.service.js
@@ -102,17 +102,32 @@ const updateMany = async (ids, completed) => {
 
 const remove = async id => {
   await DB.query(
-    `DELETE FROM todos WHERE id = $1;
+    `
+      DELETE FROM todos WHERE id = $1;
     `,
     [id],
   );
 };
 
 const removeMany = async ids => {
+  if (ids.some(id => !isUUID(id))) {
+    throw new Error('Invalid ID format');
+  }
+
   await DB.query(
-    `DELETE FROM todos WHERE id IN (${ids.map(id => `'${id}'`).join(', ')});`,
+    `
+      DELETE FROM todos
+      WHERE id IN (${ids.map(id => `'${id}'`).join(', ')});
+    `,
   );
 };
+
+function isUUID(str) {
+  const uuidRegex =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
+  return uuidRegex.test(str);
+}
 
 module.exports = {
   getAll,
